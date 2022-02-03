c99dc78a007fad4bbfc72bc6f816d0c4f4a219d0
Mikhail.Chernichenko <mchernichenko@mail.ru>
2022-02-04 00:46:10 +0300

    fix checkStyle error

--- src/main/java/hexlet/code/dto/TaskDto.java
+++ src/main/java/hexlet/code/dto/TaskDto.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 @Setter
