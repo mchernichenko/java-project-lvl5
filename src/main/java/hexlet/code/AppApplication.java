a369883a54fd101c207a421958e9f18cccce3636
Mikhail.Chernichenko <mchernichenko@mail.ru>
2022-02-07 00:22:56 +0300

    fix checkStyle error

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -8,23 +8,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @OpenAPIDefinition(
-		info = @Info(
-				title = "Task Manager OpenAPI definition",
-				version = "1.0",
-				description = " API для работы с пользователями, задачами и справочниками: "
-						+ "просмотр задач, фильтрация задач, назначение задач пользователям,"
-						+ " изменение и удаление задач.\n"
-						+ "CRUD операции для атрибутов задач: пользователи, статусы, метки.",
-				contact = @Contact(
-						name = "Mikhail Chernichenko",
-						email = "mchernichenko@mail.ru"
-				)
-		),
-		security = @SecurityRequirement(name = "bearerAuth")
+        info = @Info(
+                title = "Task Manager OpenAPI definition",
+                version = "1.0",
+                description = " API для работы с пользователями, задачами и справочниками: "
+                        + "просмотр задач, фильтрация задач, назначение задач пользователям,"
+                        + " изменение и удаление задач.\n"
+                        + "CRUD операции для атрибутов задач: пользователи, статусы, метки.",
+                contact = @Contact(
+                        name = "Mikhail Chernichenko",
+                        email = "mchernichenko@mail.ru"
+                )
+        ),
+        security = @SecurityRequirement(name = "bearerAuth")
 )
 @SpringBootApplication
 public class AppApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(AppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
 }
